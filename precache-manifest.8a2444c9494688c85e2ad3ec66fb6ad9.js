082ea421a9412d5095ce8f59f6400d7675ffc93e
Shivanshi <1999shivanshi.patel@gmail.com>
2021-01-08 18:00:33 +0530

    Updates

---
+++ precache-manifest.8a2444c9494688c85e2ad3ec66fb6ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e5646b5963c241fa7229110911c3b0",
+    "url": "/QuizWeb/index.html"
+  },
+  {
+    "revision": "cd673af80e6d49ca0f2f",
+    "url": "/QuizWeb/static/css/main.55b66aa2.chunk.css"
+  },
+  {
+    "revision": "af1d602d3c96617142f1",
+    "url": "/QuizWeb/static/js/2.35fcb30a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/QuizWeb/static/js/2.35fcb30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd673af80e6d49ca0f2f",
+    "url": "/QuizWeb/static/js/main.7b8ec210.chunk.js"
+  },
+  {
+    "revision": "df3dfeb4c666c9a9e442",
+    "url": "/QuizWeb/static/js/runtime-main.c98cbc6e.js"
+  }
+]);
