2ba2a852cce5ceda6764c8724c0d570b577555e7
Shivanshi <1999shivanshi.patel@gmail.com>
2021-01-08 17:39:36 +0530

    Initial commit

---
+++ src/QuizData.js
@@ -0,0 +1,62 @@
+export const QuizData=[
+    {
+        id:0,
+        question:`What is Capital of India?`,
+        options:[`Mumbai`,`New Delhi`,`Agra`,`Kolkata`],
+        answer:`New Delhi`
+    },
+    {
+        id:1,
+        question:`What is Capital of Nigeria?`,
+        options:[`New Delhi`,`Abuja`,`Aba`,`Kolkata`],
+        answer:`Abuja`
+    },
+    {
+        id:2,
+        question:`What is Capital of Australia?`,
+        options:[`Mumbai`,`Abuja`,`Warri`,`Sydney`],
+        answer:`Sydney`
+    },
+    {
+        id:3,
+        question:`Akara is the capital of which country?`,
+        options:[`Uruguay`,`Turkey`,`Uganda`,`Vanuatu`],
+        answer:`Turkey`
+    },
+    {
+        id:4,
+        question:`What is the capital of Finland?`,
+        options:[`Conakry`,`Prague`,`Helsinki`,`None of these`],
+        answer:`Abuja`
+    },
+    {
+        id:5,
+        question:`What is Capital of Syria?`,
+        options:[`Rimadi`,`Damascus`,`Owerri`,`Tel Aviv`],
+        answer:`Damascus`
+    },
+    {
+        id:6,
+        question:`Hitler party which came into power in 1933 is known as`,
+        options:[`Labour party`,`Nazi Party`,`Ku-klux-Klan`,`Democratic party`],
+        answer:`Nazi Party`
+    },
+    {
+        id:7,
+        question:`Epsom is the place associated with`,
+        options:[`Horse Racing`,`Polo`,`Shooting`,`Snooker`],
+        answer:`Horse racing`
+    },
+    {
+        id:8,
+        question:`Golf player Vijay Singh belongs to which country?`,
+        options:[`USA`,`Fiji`,`India`,`UK`],
+        answer:`Fiji`
+    },
+    {
+        id:9,
+        question:`First Afghan War took place in`,
+        options:[`1839`,`1843`,`1833`,`1848`],
+        answer:`1839`
+    },
+]
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Quiz from './Quiz'
+function App(){
+  return(
+    <div>
+           <Quiz/>
+    </div>
+  )
+}
+export default App;
+const rootElement=document.getElementById('root')
+ReactDOM.render(<App/>,rootElement)
